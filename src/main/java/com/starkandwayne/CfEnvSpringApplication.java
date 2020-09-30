14286e1c7e34f7cc81d5263a696b4bc6627fb212
Jonathan Pahl <jdpahl122@gmail.com>
2020-09-30 15:36:07 -0400

    Update manifest and endpoints

--- src/main/java/com/starkandwayne/CfEnvSpringApplication.java
+++ src/main/java/com/starkandwayne/CfEnvSpringApplication.java
@@ -1,8 +1,12 @@
 package com.starkandwayne;
 
+import java.util.Map;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,8 +14,27 @@ import org.springframework.web.bind.annotation.RestController;
 public class CfEnvSpringApplication {
 	
 	@GetMapping("/")
-	String home() {
-		return System.getenv().toString();
+	public String home() {
+		
+        Map<String, String> env = System.getenv();
+        
+        String string = "<head><title>Cloud Foundry Environment</title><meta name=\\\"viewport\\\" content=\\\"width=device-width\\\"></head>";
+        string = string + "<h2>Cloud Foundry Environment</h2>";
+        
+        string = string + "<div><table>";
+        for (String envName : env.keySet()) {
+            string = string + "<tr><td><strong>" + envName + "</td></strong>" + "<td>" + env.get(envName) + "</td>" + "</tr>";
+        }
+		
+        string = string + "</table></div>";
+        
+		return string;
+	}
+	
+	@RequestMapping("/{var}")
+	public String envVariable(@PathVariable("var") String var) {
+        Map<String, String> env = System.getenv();
+		return env.get(var);
 	}
 
 	public static void main(String[] args) {
